f4f722a140421701419f4e3e958dba09f1da211e
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-26 10:32:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,7 @@
-const CACHE_NAME = "spiritual-Word";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./learn.pdf",
-  "./https://i.imgur.com/mwkZeR2.jpeg",
-  "./https://i.imgur.com/mwkZeR2.jpeg"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("activate", () => {
   self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
 });
