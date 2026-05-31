9f437d42f593782f881f2d1cd76e27593358e696
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-31 11:14:44 +0300

    Update sponsors.js

--- sponsors.js
+++ sponsors.js
@@ -2,6 +2,6 @@ const sponsors = [
   {
     id: 1,
     title: "In His Words. We Walk In Perfection",
-    video: "https://derrickafricatz-wq.github.io/Holly-Echo/video/com.mp4"
+    video: "video/com.mp4"
   }
 ];
